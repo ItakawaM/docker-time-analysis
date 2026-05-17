f4dc156f2ffcf35b7100d62cef817d4c984d60fa
ItakawaM <maksymworkp@gmail.com>
2026-05-17 21:05:26 +0300

    feat(compute): remake the piecewise regression model into a Linear-Linear model

--- backend/internal/compute/regression.go
+++ backend/internal/compute/regression.go
@@ -7,6 +7,7 @@ type Regression interface {
 }
 
 type LinearRegression struct {
+	Type             string  `json:"type"`
 	AlphaCoefficient float64 `json:"alphaCoefficient"`
 	BetaCoefficient  float64 `json:"betaCoefficient"`
 	RSquared         float64 `json:"rSquared"`
@@ -18,24 +19,26 @@ func (lf *LinearRegression) Predict(x float64) float64 {
 }
 
 type PiecewiseRegression struct {
-	Breakpoint                  float64 `json:"breakpoint"`
-	LinearAlphaCoefficient      float64 `json:"linearAlphaCoefficient"`
-	LinearBetaCoefficient       float64 `json:"linearBetaCoefficient"`
-	ExponentialAlphaCoefficient float64 `json:"exponentialAlphaCoefficient"`
-	ExponentialBetaCoefficient  float64 `json:"exponentialBetaCoefficient"`
-	RSquared                    float64 `json:"rSquared"`
-	Qo                          float64 `json:"qo"`
+	Type                  string  `json:"type"`
+	Breakpoint            float64 `json:"breakpoint"`
+	LeftAlphaCoefficient  float64 `json:"leftAlphaCoefficient"`
+	LeftBetaCoefficient   float64 `json:"leftBetaCoefficient"`
+	RightAlphaCoefficient float64 `json:"rightAlphaCoefficient"`
+	RightBetaCoefficient  float64 `json:"rightBetaCoefficient"`
+	RSquared              float64 `json:"rSquared"`
+	Qo                    float64 `json:"qo"`
 }
 
 func (pf PiecewiseRegression) Predict(x float64) float64 {
 	if x <= pf.Breakpoint {
-		return pf.LinearAlphaCoefficient*x + pf.LinearBetaCoefficient
+		return pf.LeftAlphaCoefficient*x + pf.LeftBetaCoefficient
 	}
 
-	return pf.ExponentialBetaCoefficient * math.Pow(pf.ExponentialAlphaCoefficient, x)
+	return pf.RightAlphaCoefficient*x + pf.RightBetaCoefficient
 }
 
 type ExponentialRegression struct {
+	Type             string  `json:"type"`
 	AlphaCoefficient float64 `json:"alphaCoefficient"`
 	BetaCoefficient  float64 `json:"betaCoefficient"`
 	RSquared         float64 `json:"rSquared"`
